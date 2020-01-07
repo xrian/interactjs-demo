0cb9d182e909aad5d5046ae208a21490f7390563
张淞 <iocntop@gmail.com>
2020-01-08 01:51:03 +0800

    no message

--- js/draggable.js
+++ js/draggable.js
@@ -4,6 +4,10 @@
 
 const position = { x: 0, y: 0 };
 
+// 设置可以拖动的空间
+//interact('.body').dropzone({});
+
+
 interact('.draggable').draggable({
   // 拖动使用惯性
   inertia: false,
@@ -12,26 +16,41 @@ interact('.draggable').draggable({
   // 忽略 button 上面的拖动事件
   ignoreFrom: 'button',
   modifiers: [
-    interact.modifiers.restrictRect({
-      restriction: 'parent',
-      endOnly: true
-    })
+//    interact.modifiers.restrictRect({
+//      restriction: 'parent',
+//      endOnly: true
+//    })
   ],
-  listeners: {
-    start(event) {
-      console.log(event.type, event.target);
-    },
-    move(event) {
-      position.x += event.dx;
-      position.y += event.dy;
+  onmove: function(event){
+    var target = event.target
+    // keep the dragged position in the data-x/data-y attributes
+    var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+    var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
 
-      event.target.style.transform =
-        `translate(${position.x}px, ${position.y}px)`;
-    },
+    // translate the element
+    target.style.webkitTransform =
+      target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)'
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x)
+    target.setAttribute('data-y', y)
   },
+//  listeners: {
+//    start(event) {
+//      console.log(event.type, event.target);
+//    },
+//    move(event) {
+//      position.x += event.dx;
+//      position.y += event.dy;
+//
+//      event.target.style.transform =
+//        `translate(${position.x}px, ${position.y}px)`;
+//    },
+//  },
   // 拖动结束后执行
   onend: function (event) {
-    var textEl = event.target.querySelector('area');
+    var textEl = event.target.querySelector('#main');
 
     textEl && (textEl.textContent =
       'moved a distance of ' +
@@ -39,6 +58,6 @@ interact('.draggable').draggable({
         Math.pow(event.pageY - event.y0, 2) | 0))
         .toFixed(2) + 'px')
   }
-}).dropzone({
-  overlap: 0.5
-});
+})
+//  .dropzone({})
+  .dynamicDrop(true);
